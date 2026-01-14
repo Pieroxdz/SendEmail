f2552a6c16bea3b34e6d43b305ae645d70a1c5b0
Piero <markinpiero005@gmail.com>
2026-01-13 21:17:55 -0500

    validación implementada en la UX con alertas

--- js/app.js
+++ js/app.js
@@ -3,16 +3,49 @@ document.addEventListener("DOMContentLoaded", () => {
     const inputEmail = document.querySelector("#email");
     const inputCc = document.querySelector("#cc");
     const inputAsunto = document.querySelector("#asunto");
-    const mensaje = document.querySelector("#mensaje");
     const btnSubtmit = document.querySelector("#botones button[type='submit']");
 
+
+
+    const validarEmail = (email) => {
+        const regex = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/
+        const resultado = regex.test(email);
+        return resultado
+    }
+
+    const limpiarAlerta = (referencia) => {
+        const alerta = referencia.querySelector(".bg-red-600");
+
+        if (alerta) {
+            alerta.remove()
+        }
+    }
+
+    const mostrarAlerta = (mensaje, referencia) => {
+        limpiarAlerta(referencia)
+
+        const error = document.createElement("P");
+        error.textContent = mensaje;
+        error.classList.add("bg-red-600", "text-white", "p-2");
+
+        referencia.appendChild(error);
+    }
+
+
     const validar = (e) => {
+        //PARA TODOS LOS INPUTS
         if (e.target.value.trim() === "") {
-            console.log("está vacio");
+            mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement)
             return;
         }
 
-        console.log("si hay algo....");
+        //SOLO PARA EL INPUT DE EMAIL
+        if (e.target.id === "email" && !validarEmail(e.target.value)) {
+            mostrarAlerta("El email no es válido", e.target.parentElement)
+            return;
+        }
+
+        limpiarAlerta(e.target.parentElement);
     }
 
     //EVENTOS
